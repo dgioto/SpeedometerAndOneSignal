4281ab4e81553912b58e5d22ef851cc4b002d4a1
Alexander Tih <dgioto@gmail.com>
2023-11-10 11:48:28 +0200

    added touch to the third and fourth buttons

--- app/src/main/java/com/dgioto/speedometerandonesignal/view/CustomView.kt
+++ app/src/main/java/com/dgioto/speedometerandonesignal/view/CustomView.kt
@@ -9,18 +9,14 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 
-//add the code for pressing the GREEN and YELLOW button
 class CustomView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
     private val paint = Paint()
     private val paintC = Paint()
+    // Угол начала рисования секторов (в градусах)
     private val startAngle = -180f
-    private val colors = listOf(
-        Color.RED,
-        Color.BLUE,
-        Color.GREEN,
-        Color.YELLOW
-    )
+    private val colors = listOf(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW)
+    // Угол, на который делится круг для каждого цвета
     private val sweepAngle = 360f / colors.size
     private var buttonClicked = -1
 
@@ -43,9 +39,9 @@ class CustomView(context: Context, attributeSet: AttributeSet) : View(context, a
         val radius = width.coerceAtMost(height) / 2f
 
         for (i in colors.indices) {
-            paintC.color = if (i == buttonClicked)
-                Color.GRAY
-            else colors[i]
+            paintC.color =
+                if (i == buttonClicked) Color.GRAY
+                else colors[i]
             canvas.drawArc(
                 centerX - radius,
                 centerY - radius,
@@ -61,36 +57,31 @@ class CustomView(context: Context, attributeSet: AttributeSet) : View(context, a
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        val centerX = width / 2f
-        val centerY = width / 2f
         val x = event.x
         val y = event.y
+        val centerX = width / 2f
+        val centerY = width / 2f
 
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 //click on RED
-                if (x < centerX && y < centerY){
-                    buttonClicked = 0
-                }
+                if (x < centerX && y < centerY) buttonClicked = 0
                 //click on BLUE
-                if (x > centerX && y < centerY){
-                    buttonClicked = 1
-                }
-//                //click on GREEN
-//                if (x >  && y < ){
-//
-//                }
-//                //click on YELLOW
-//                if (x >  && y < ){
-//
-//                }
+                if (x > centerX && y < centerY) buttonClicked = 1
+                //click on GREEN
+                if (x > centerX && y > centerY) buttonClicked = 2
+                //click on YELLOW
+                if (x < centerX && y > centerY) buttonClicked = 3
+                // Перерисовка вида
                 invalidate()
             }
+            // Сброс выбранной кнопки при отпускании экрана
             MotionEvent.ACTION_UP -> {
                 buttonClicked = -1
                 invalidate()
             }
         }
+        // Возвращение true, чтобы показать, что событие обработано
         return true
     }
 }
