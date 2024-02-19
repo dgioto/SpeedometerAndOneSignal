f56b21a90945f73eb16942f04177e706be50a250
Alexander Tih <dgioto@gmail.com>
2024-02-19 14:29:33 +0200

    adding a picture and changing the color of the buttons

--- app/src/main/java/com/dgioto/speedometerandonesignal/view/CustomView.kt
+++ app/src/main/java/com/dgioto/speedometerandonesignal/view/CustomView.kt
@@ -2,18 +2,18 @@ package com.dgioto.speedometerandonesignal.view
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
-import android.os.Build
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.dgioto.speedometerandonesignal.R
 import com.dgioto.speedometerandonesignal.TextUtils
-import org.checkerframework.checker.units.qual.Angle
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sin
@@ -25,9 +25,14 @@ class CustomView(context: Context, attributeSet: AttributeSet)
     private val paintCWidth = 170f
     private val paintText = Paint()
     private val paint = Paint()
+    private val paintBm = Paint()
     private val paintC = Paint()
     // Угол начала рисования секторов (в градусах)
     private val startAngle = 0f
+    private var mainColor = Color.BLUE
+    private var mainColor2 = Color.BLUE
+
+    private var bm: Bitmap
     private val colors = listOf(
         Color.RED,
         Color.DKGRAY,
@@ -42,6 +47,20 @@ class CustomView(context: Context, attributeSet: AttributeSet)
     private var buttonClicked = -1
 
     init {
+        val drawable = ContextCompat.getDrawable(context, R.drawable.baseline_local_florist_24)
+        bm = Bitmap.createBitmap(
+            150,
+            150,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bm)
+        canvas.rotate(-90f, canvas.width / 2f, canvas.height / 2f)
+        drawable?.setBounds(0, 0, 150, 150)
+        drawable?.draw(canvas)
+
+        mainColor = ContextCompat.getColor(context, R.color.main_menu_color)
+        mainColor2 = ContextCompat.getColor(context, R.color.main_menu_color2)
+
         paintText.style = Paint.Style.FILL
         paintText.color = Color.WHITE
         paintText.textSize = 35f
@@ -68,8 +87,8 @@ class CustomView(context: Context, attributeSet: AttributeSet)
 
         for (i in colors.indices) {
             paintC.color =
-                if (i == buttonClicked) Color.GRAY
-                else colors[i]
+                if (i == buttonClicked) Color.BLACK
+                else mainColor
             canvas.drawArc(
                 centerX - radius,
                 centerY - radius,
@@ -82,12 +101,19 @@ class CustomView(context: Context, attributeSet: AttributeSet)
             )
         }
         paintC.style = Paint.Style.FILL
-        paintC.color = Color.GRAY
+        paintC.color = mainColor2
         canvas.drawCircle(
             centerX ,
             centerY ,
-            radius / 1.7f,
+            radius / 1.5f,
             paintC
+        )
+
+        canvas.drawBitmap(
+            bm,
+            centerX - bm.width / 2f,
+            centerY - bm.height / 2f,
+            paintBm
         )
     }
 
